8089d826390fd0ace04738375e94a741e52e7607
Alex <alexis.autres@gmail.com>
2024-12-21 11:49:17 +0100

    Mise à jour du texte

--- component/alert/Alert.tsx
+++ component/alert/Alert.tsx
@@ -7,8 +7,8 @@ const Alert: React.FC = () => {
     <div className="alertBanner">
       <div className="py-2">
         <span className="block sm:inline ml-2">
-          📢 Notre restaurant sera fermé du <strong>lundi 23 au 25 Décenbre inclus 🎄</strong>, <br></br>
-          et le <strong>lundi 30 au 01 Janvier 2025 inclus 🎆</strong>. <span className="kf-pulse-red">Bonnes fêtes et à bientôt chez Pizza du Semnoz ! 🎉</span>
+          📢 Notre restaurant sera fermé du <strong>lundi 23 au 25 Décenbre inclus 🎄</strong>, et le <strong>lundi 30 au 01 Janvier 2025 inclus 🎆</strong>.{' '}
+          <span className="kf-pulse-red">Bonnes fêtes et à bientôt chez Pizza du Semnoz ! 🎉</span>
           {/* <strong>
             <Link href="/menu" className="kf-pulse-red">
               La Yaute et Tartidiots
